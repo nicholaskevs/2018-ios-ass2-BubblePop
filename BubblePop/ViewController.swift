e505951efb191fee5fb0beac5c0ad9bb12721900
Nicholas <nicholas.k.santosa@student.uts.edu.au>
2018-04-14 22:26:48 +1000

    Add assignment 2 template

---
+++ BubblePop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BubblePop
+//
+//  Created by Nicholas on 14/4/18.
+//  Copyright © 2018 Nicholas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
